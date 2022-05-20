7fb5ee0fd5f8183a5df37d91fb18959c74d1c038
simplitom <ir.tomjans@gmail.com>
2022-05-20 17:17:23 +0200

    added rules for local neighbourhood

--- LocalNeighbourhoodModule.py
+++ LocalNeighbourhoodModule.py
@@ -10,3 +10,19 @@ class LocalNeighborhood:
         output = output - self.mylist[4]
         return output
 
+    def liveCell(self):
+        return self.mylist[4]==1
+
+    def next_tick_alive(self):
+        nr_of_neighbours = self.countNeighbors()
+        if self.liveCell():
+            if nr_of_neighbours== 2 or nr_of_neighbours== 3:
+                return True
+            else:
+                return False
+        else:
+            if nr_of_neighbours == 3:
+                return True
+            else:
+                return False
+
--- LocalNeighbourhood_test.py
+++ LocalNeighbourhood_test.py
@@ -4,12 +4,35 @@ import LocalNeighbourhoodModule
 
 class testLocalNeighbourhood(unittest.TestCase):
 
-    def test_LocalNeighbourhoodtest(self):
-        ln = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 0,1,1,0,1,0,0))
+    def test_count_LocalNeighbours(self):
+        ln = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 0, 1, 1, 0, 1, 0, 0))
         self.assertEqual(ln.countNeighbors(), 3)
-        ln2 = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 0,1,0,0,1,0,0))
+        ln2 = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 0, 1, 0, 0, 1, 0, 0))
         self.assertEqual(ln2.countNeighbors(), 3)
 
+    def test_alive(self):
+        ln = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 0, 1, 1, 0, 1, 0, 0))
+        self.assertTrue(ln.liveCell())
+        ln2 = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 0, 1, 0, 0, 1, 0, 0))
+        self.assertFalse(ln2.liveCell())
+
+    def test_underpopulation_rule(self):
+        ln = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 0, 0, 1, 0, 0, 0, 0))
+        self.assertFalse(ln.next_tick_alive())
+
+    def test_next_generation_rule(self):
+        ln = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 1, 0, 1, 0, 0, 0, 1))
+        self.assertTrue(ln.next_tick_alive())
+
+    def test_overpopulation_rule(self):
+        ln = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 1, 0, 1, 1, 1, 0, 1))
+        self.assertFalse(ln.next_tick_alive())
+
+    def test_reproduction_rule(self):
+        ln = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 1, 0, 0, 1, 1, 0, 1))
+        self.assertFalse(ln.next_tick_alive())
+        ln = LocalNeighbourhoodModule.LocalNeighborhood((1, 0, 1, 0, 0, 0, 0, 0, 1))
+        self.assertTrue(ln.next_tick_alive())
 
 if __name__ == '__main__':
     unittest.main()
